f76b935ce6b3432f47347b79916289803bbcbf1a
ntbased <33110744+ntbased@users.noreply.github.com>
2018-04-22 14:27:31 -0700

    Added Color chase and white stripe animations to LED strip

--- Main/Main.ino
+++ Main/Main.ino
@@ -35,12 +35,12 @@ Keypad keypd = Keypad( makeKeymap(buttons), rowPins, colPins, ROWS, COLS );
 #define PIN            A0
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      16
+#define NUM_LED      30
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
 // example for more information on possible values.
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LED, PIN, NEO_GRB + NEO_KHZ800);
 
 // Set animation speed
 const int animationMultiplier = 100;
@@ -48,18 +48,28 @@ const int animationMultiplier = 100;
 // Set up a timer
 int timer = 0;
 
+//
+#define QUEUE_LEN 20
+
 // Set up a list for each key pressed
-char keys[10] = {'0','0','0','0','0','0','0','0','0','0'};
+char keys[QUEUE_LEN];
 
 // Set up a list for animation step for each key
-int steps[10] = {0,0,0,0,0,0,0,0,0,0};
+int steps[QUEUE_LEN];
 
 int spiralArray[64][2] = {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}, {0, 6}, {0, 7}, {1, 7}, {2, 7}, {3, 7}, {4, 7}, {5, 7}, {6, 7}, {7, 7}, {7, 6}, {7, 5}, {7, 4}, {7, 3}, {7, 2}, {7, 1}, {7, 0}, {6, 0}, {5, 0}, {4, 0}, {3, 0}, {2, 0}, {1, 0}, {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6}, {2, 6}, {3, 6}, {4, 6}, {5, 6}, {6, 6}, {6, 5}, {6, 4}, {6, 3}, {6, 2}, {6, 1}, {5, 1}, {4, 1}, {3, 1}, {2, 1}, {2, 2}, {2, 3}, {2, 4}, {2, 5}, {3, 5}, {4, 5}, {5, 5}, {5, 4}, {5, 3}, {5, 2}, {4, 2}, {3, 2}, {3, 3}, {3, 4}, {4, 4}, {4, 3}};
 
 void setup() {
-
   // Activate console logging
   Serial.begin(9600);
+
+  for (int i = 0; i < QUEUE_LEN; i++){
+    keys[i] = '0';
+  }
+
+  for(int i = 0; i < QUEUE_LEN; i++){
+    steps[i] = 0;
+  }
   
   /*
   The MAX72XX is in power-saving mode on startup,
@@ -71,6 +81,7 @@ void setup() {
   /* and clear the display */
   lc.clearDisplay(0);
 
+  strip.setBrightness(50);
   strip.begin();
   strip.show();
 }
@@ -80,32 +91,48 @@ void loop() {
   char key = keypd.getKey();
   
   if (key) {
+    //Add to display engine
     addKey(key);
   }
 
   if (timer % 100 == 0) {
     lc.clearDisplay(0);
     // Iterates throughout the list and goes frame by frame
-    for (int i = 0; i < 10; i++) {
-      if (keys[i] == '1') {
+    for (int i = 0; i < QUEUE_LEN; i++) {
+      if (keys[i] == '1' ) {
         laserIn(steps[i], i);
-      }
-      
-      else if (keys[i] == '2') {
+        
+      }else if (keys[i] == '2') {
         laserOut(steps[i], i);
-      }
-      
-      else if (keys[i] == '3') {
+        
+      }else if (keys[i] == '3') {
         crossIn(steps[i], i);
-      }
-      
-      else if (keys[i] == '4') {
+        
+      }else if (keys[i] == '4') {
         crossOut(steps[i], i);
+        
       }else if (keys[i] == '5') {
         spiralIn(steps[i], i);
+        
       }
   
       // To add more keys, add an else if statement, then feed in steps[i] and i
+    }
+  }
+
+  if (timer % 50 == 0) {
+    clearStrip();
+    strip.show();
+    for(int i = 0; i < QUEUE_LEN; i++){
+      if (keys[i] == '6') {
+        stripComet(steps[i], i);
+        
+      }else if (keys[i] == '7'){
+        stripColorChase(steps[i], i);
+        
+      }else if (keys[i] == '8'){
+        stripWhiteFill(steps[i], i);
+      }
     }
   }
 
@@ -115,7 +142,7 @@ void loop() {
 
 void addKey(char key) {
   // Iterates through all instances in the active animations list
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < QUEUE_LEN; i++) {
     
     if (keys[i] == '0') {
       
@@ -243,3 +270,81 @@ void spiralIn(int aniStep, int keysId) {
     // Resets list
   }
 }
+
+void stripComet(int aniStep, int keysId){
+  for(int i = 0; i < 3; i++){
+    strip.setPixelColor(aniStep-1, 255, 255, 255);
+  }
+
+  strip.show();
+  
+  // Adds to the frame count
+  steps[keysId] += 1;
+
+  // If the the frame count reaches the end of the animation, reset everything
+  if (aniStep == NUM_LED-1) { // The number here should correlate to the end of the frame loop
+    keys[keysId] = '0';
+    steps[keysId] = 0;
+    // Resets list
+  }
+}
+
+void stripColorChase(int aniStep, int keysId){
+  //Paints the green
+  for(int i = 0; i <= aniStep && i < NUM_LED; i++){
+    strip.setPixelColor(i, 0, 0, 255);
+  }
+
+  //Paints the blue
+  for(int i = 0; i <= aniStep-NUM_LED && i < NUM_LED; i++){
+    strip.setPixelColor(i, 0, 255, 0);
+  }
+
+  //Paints red
+  for(int i = 0; i <= aniStep-2*NUM_LED && i < NUM_LED; i++){
+    strip.setPixelColor(i, 255, 0, 0);
+  }
+
+  //Clears tail
+  for(int i = 0; i <= aniStep-3*NUM_LED && i < NUM_LED; i++){
+    strip.setPixelColor(i, 0, 0, 0);
+  }
+
+  //Shows colors
+  strip.show();
+  
+  // Adds to the frame count
+  steps[keysId] += 1;
+
+  // If the the frame count reaches the end of the animation, reset everything
+  if (aniStep == 4*NUM_LED-1) { // The number here should correlate to the end of the frame loop
+    keys[keysId] = '0';
+    steps[keysId] = 0;
+    // Resets list
+  }
+}
+
+void stripWhiteFill(int aniStep, int keysId){
+  for(int i = 0; i <= aniStep; i++){
+    strip.setPixelColor(i, 255, 250, 245);
+  }
+
+  strip.show();
+  
+  // Adds to the frame count
+  steps[keysId] += 1;
+
+  // If the the frame count reaches the end of the animation, reset everything
+  if (aniStep == NUM_LED-1) { // The number here should correlate to the end of the frame loop
+    keys[keysId] = '0';
+    steps[keysId] = 0;
+    // Resets list
+  }
+}
+
+void clearStrip(){
+  for(int i = 0; i < NUM_LED; i++){
+    strip.setPixelColor(i, 0, 0, 0);
+  }
+}
+
