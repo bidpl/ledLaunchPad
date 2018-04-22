0af7f09ab65b8a0824edbb517218e9667eb26aed
Binh-Minh Nguyen <binhminh292@gmail.com>
2018-04-22 03:08:02 -0700

    added spiral

--- ledMatrix/ledMatrix.ino
+++ ledMatrix/ledMatrix.ino
@@ -1,4 +1,3 @@
-#include <Keypad.h>
 #include <LedControl.h>
 
 /*
@@ -9,49 +8,18 @@
  pin 10 is connected to LOAD 
  We have only a single MAX72XX.
  */
-LedControl lc = LedControl(12,11,10,1);
+LedControl lc=LedControl(12,11,10,1);
 
-const byte ROWS = 4; // Four rows
-const byte COLS = 4; // Three columns
-// Define the Keymap
-char buttons[ROWS][COLS] = {
-  {'1','2','3','4'},
-  {'5','6','7','8'},
-  {'9','0','A','B'},
-  {'C','D','E','F'}
-};
-// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
-byte rowPins[ROWS] = { 7, 6, 8, 9 };
-// Connect keypad COL0, COL1 and COL2 to these Arduino pins.
-byte colPins[COLS] = { 5, 4, 3, 2 };
-
-// Create the Keypad
-Keypad keypd = Keypad( makeKeymap(buttons), rowPins, colPins, ROWS, COLS );
-
-// Set animation speed
 const int animationMultiplier = 100;
 
-// Set up a timer
-int timer = 0;
-
-// Set up a list for each key pressed
-char keys[10] = {'0','0','0','0','0','0','0','0','0','0'};
-
-// Set up a list for animation step for each key
-int steps[10] = {0,0,0,0,0,0,0,0,0,0};
-
 void setup() {
-
-  // Activate console logging
-  Serial.begin(9600);
-  
   /*
   The MAX72XX is in power-saving mode on startup,
   we have to do a wakeup call
   */
   lc.shutdown(0,false);
   /* Set the brightness to a medium values */
-  lc.setIntensity(0,1);
+  lc.setIntensity(0,8);
   /* and clear the display */
   lc.clearDisplay(0);
 
@@ -59,172 +27,80 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  char key = keypd.getKey();
   
-  if (key == '3') {
-    addKey(key);
-  }
+  laserIn(animationMultiplier);
+  crossOut(animationMultiplier);
+  crossIn(animationMultiplier);
+  laserOut(animationMultiplier);
+}
 
-  for (int i = 0; i < 10; i++) {
-    if (keys[i] == '1') {
-      laserIn(steps[i], keys[i]);
-    }
-    
-    else if (keys[i] == '2') {
-      laserOut(steps[i], keys[i]);
-    }
-    
-    else if (keys[i] == '3') {
-      crossIn(steps[i], keys[i]);
-    }
-    
-    else if (keys[i] == '4') {
-      crossOut(steps[i], keys[i]);
-    }
-  }
-
-  delay(100);
-
-  lc.clearDisplay(0);
-
-  timer++;
-  /*
-  if(key)  // Check for a valid key.
-  {
-    Serial.println(key);
-
-    switch (key){
-      case '1':
-        laserIn(animationMultiplier);
-        break;
-        
-      case '2':
-        laserOut(animationMultiplier);
-        break;
-
-      case '3':
-        crossIn(animationMultiplier);
-        break;
-
-      case '4':
-        crossOut(animationMultiplier);
-        break;
-
-      default:
-        break;
-    }
-
+void crossIn(int aniSpeed){
+  for(int i = 0; i < 4; i++){
     lc.clearDisplay(0);
-  }*/
-}
-
-void addKey(char key) {
-  // Iterates through all instances in the active animations list
-  for (int i = 0; i < 10; i++) {
+    lc.setLed(0, i, i, true);
+    lc.setLed(0, 7-i, i, true);
+    lc.setLed(0, i, 7-i, true);
+    lc.setLed(0, 7-i,7-i, true);
     
-    if (keys[i] != '0') {
-      
-      keys[i] = key;
-      // If the instance in the keys list is not being used, assign a key to that list
-      
-    }
+    delay(aniSpeed);
+  }
+}
+
+void crossOut(int aniSpeed){
+  for(int i = 3; i >= 0; i--){
+    lc.clearDisplay(0);
+    lc.setLed(0, i, i, true);
+    lc.setLed(0, 7-i, i, true);
+    lc.setLed(0, i, 7-i, true);
+    lc.setLed(0, 7-i,7-i, true);
     
+    delay(aniSpeed);
   }
 }
 
-void crossIn(int aniStep, int keysId){
-  lc.setLed(0, aniStep, aniStep, true);
-  lc.setLed(0, 7-aniStep, aniStep, true);
-  lc.setLed(0, aniStep, 7-aniStep, true);
-  lc.setLed(0, 7-aniStep,7-aniStep, true);
+void laserIn(int aniSpeed){
+  for(int i = 0; i < 4; i++){
+    lc.clearDisplay(0);
+    //Vertical Up
+    lc.setLed(0, 3, i, true);
+    lc.setLed(0, 4, i, true);
 
-    // Adds to the frame count
-  steps[keysId] += 1;
+    //Vertical Down
+    lc.setLed(0, 3, 7-i, true);
+    lc.setLed(0, 4, 7-i, true);
 
-  // If the the frame count reaches the end of the animation, reset everything
-  if (aniStep == 8) {
-    keys[keysId] = '0';
-    steps[keysId] = 0;
+    //Left in
+    lc.setLed(0, i, 3, true);
+    lc.setLed(0, i, 4, true);
+
+    //Vertical Up
+    lc.setLed(0, 7-i, 3, true);
+    lc.setLed(0, 7-i, 4, true);
+
+    delay(aniSpeed);
   }
-  
-}
-
-void crossOut(int aniStep, int keysId){
-  lc.setLed(0, aniStep, aniStep, true);
-  lc.setLed(0, 7-aniStep, aniStep, true);
-  lc.setLed(0, aniStep, 7-aniStep, true);
-  lc.setLed(0, 7-aniStep,7-aniStep, true);
-
-    // Adds to the frame count
-  steps[keysId] += 1;
-
-  // If the the frame count reaches the end of the animation, reset everything
-  if (aniStep == 8) {
-    keys[keysId] = '0';
-    steps[keysId] = 0;
-  }
-  
-}
-
-void laserIn(int aniStep, int keysId){
-  //Vertical Up
-  lc.setLed(0, 3, aniStep, true);
-  lc.setLed(0, 4, aniStep, true);
-
-  //Vertical Down
-  lc.setLed(0, 3, 7-aniStep, true);
-  lc.setLed(0, 4, 7-aniStep, true);
-
-  //Left in
-  lc.setLed(0, aniStep, 3, true);
-  lc.setLed(0, aniStep, 4, true);
-
-  //Vertical Up
-  lc.setLed(0, 7-aniStep, 3, true);
-  lc.setLed(0, 7-aniStep, 4, true);
-
-    // Adds to the frame count
-  steps[keysId] += 1;
-
-  // If the the frame count reaches the end of the animation, reset everything
-  if (aniStep == 8) {
-    keys[keysId] = '0';
-    steps[keysId] = 0;
-  }
-  
 }
 
 
-void laserOut(int aniStep, int keysId){
-  /*
-   * Takes in a step and an ID
-   * The step is for positioning and frame count
-   * The ID is for knowing what is going on
-   */
-  
-  //Vertical Up
-  lc.setLed(0, 3, aniStep, true);
-  lc.setLed(0, 4, aniStep, true);
+void laserOut(int aniSpeed){
+  for(int i = 4; i >= 0; i--){
+    lc.clearDisplay(0);
+    //Vertical Up
+    lc.setLed(0, 3, i, true);
+    lc.setLed(0, 4, i, true);
 
-  //Vertical Down
-  lc.setLed(0, 3, 7-aniStep, true);
-  lc.setLed(0, 4, 7-aniStep, true);
+    //Vertical Down
+    lc.setLed(0, 3, 7-i, true);
+    lc.setLed(0, 4, 7-i, true);
 
-  //Left in
-  lc.setLed(0, aniStep, aniStep, true);
-  lc.setLed(0, aniStep, aniStep, true);
+    //Left in
+    lc.setLed(0, i, 3, true);
+    lc.setLed(0, i, 4, true);
 
-  //Vertical Up
-  lc.setLed(0, 7-aniStep, aniStep, true);
-  lc.setLed(0, 7-aniStep, aniStep, true);
+    //Vertical Up
+    lc.setLed(0, 7-i, 3, true);
+    lc.setLed(0, 7-i, 4, true);
 
-  // Adds to the frame count
-  steps[keysId] += 1;
-
-  // If the the frame count reaches the end of the animation, reset everything
-  if (aniStep == 8) {
-    keys[keysId] = '0';
-    steps[keysId] = 0;
+    delay(aniSpeed);
   }
-  
 }
