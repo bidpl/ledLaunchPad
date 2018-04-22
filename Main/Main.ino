79d40980ef93cca319267f60a917f376161f152d
ntbased <33110744+ntbased@users.noreply.github.com>
2018-04-22 07:34:48 -0700

    Added new spiral version

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,3 +1,5 @@
+#include <Adafruit_NeoPixel.h>
+
 #include <Keypad.h>
 #include <LedControl.h>
 
@@ -28,6 +30,18 @@ byte colPins[COLS] = { 5, 4, 3, 2 };
 // Create the Keypad
 Keypad keypd = Keypad( makeKeymap(buttons), rowPins, colPins, ROWS, COLS );
 
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1
+#define PIN            A0
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      16
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
 // Set animation speed
 const int animationMultiplier = 100;
 
@@ -39,6 +53,8 @@ char keys[10] = {'0','0','0','0','0','0','0','0','0','0'};
 
 // Set up a list for animation step for each key
 int steps[10] = {0,0,0,0,0,0,0,0,0,0};
+
+int spiralArray[64][2] = {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}, {0, 6}, {0, 7}, {1, 7}, {2, 7}, {3, 7}, {4, 7}, {5, 7}, {6, 7}, {7, 7}, {7, 6}, {7, 5}, {7, 4}, {7, 3}, {7, 2}, {7, 1}, {7, 0}, {6, 0}, {5, 0}, {4, 0}, {3, 0}, {2, 0}, {1, 0}, {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6}, {2, 6}, {3, 6}, {4, 6}, {5, 6}, {6, 6}, {6, 5}, {6, 4}, {6, 3}, {6, 2}, {6, 1}, {5, 1}, {4, 1}, {3, 1}, {2, 1}, {2, 2}, {2, 3}, {2, 4}, {2, 5}, {3, 5}, {4, 5}, {5, 5}, {5, 4}, {5, 3}, {5, 2}, {4, 2}, {3, 2}, {3, 3}, {3, 4}, {4, 4}, {4, 3}};
 
 void setup() {
 
@@ -55,88 +71,58 @@ void setup() {
   /* and clear the display */
   lc.clearDisplay(0);
 
+  strip.begin();
+  strip.show();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   char key = keypd.getKey();
   
-  if (key == '3') {
+  if (key) {
     addKey(key);
   }
 
-  for (int i = 0; i < 10; i++) {
-    if (keys[i] == '1') {
-      laserIn(steps[i], keys[i]);
-    }
-    
-    else if (keys[i] == '2') {
-      laserOut(steps[i], keys[i]);
-    }
-    
-    else if (keys[i] == '3') {
-      crossIn(steps[i], keys[i]);
-    }
-    
-    else if (keys[i] == '4') {
-      crossOut(steps[i], keys[i]);
+  if (timer % 100 == 0) {
+    lc.clearDisplay(0);
+    // Iterates throughout the list and goes frame by frame
+    for (int i = 0; i < 10; i++) {
+      if (keys[i] == '1') {
+        laserIn(steps[i], i);
+      }
+      
+      else if (keys[i] == '2') {
+        laserOut(steps[i], i);
+      }
+      
+      else if (keys[i] == '3') {
+        crossIn(steps[i], i);
+      }
+      
+      else if (keys[i] == '4') {
+        crossOut(steps[i], i);
+      }else if (keys[i] == '5') {
+        spiralIn(steps[i], i);
+      }
+  
+      // To add more keys, add an else if statement, then feed in steps[i] and i
     }
   }
 
-  delay(100);
-
-  lc.clearDisplay(0);
-
+  delay(1);
   timer++;
-  /*
-  if(key)  // Check for a valid key.
-  {
-    Serial.println(key);
-
-    switch (key){
-      case '1':
-        laserIn(animationMultiplier);
-        break;
-        
-      case '2':
-        laserOut(animationMultiplier);
-        break;
-
-      case '3':
-        crossIn(animationMultiplier);
-        break;
-
-      case '4':
-        crossOut(animationMultiplier);
-        break;
-
-      case '5':
-        spiralIn(animationMultiplier);
-        break;
-
-
-      case 'F':
-        lc.clearDisplay(0);
-        lc.setLed(0, 0, 0, true);
-        delay(1000);
-        break;
-
-      default:
-        break;
-    }
-
-    lc.clearDisplay(0);
-  }*/
 }
 
 void addKey(char key) {
   // Iterates through all instances in the active animations list
   for (int i = 0; i < 10; i++) {
     
-    if (keys[i] != '0') {
+    if (keys[i] == '0') {
       
       keys[i] = key;
       // If the instance in the keys list is not being used, assign a key to that list
+
+      break;
       
     }
     
@@ -153,7 +139,7 @@ void crossIn(int aniStep, int keysId){
   steps[keysId] += 1;
 
   // If the the frame count reaches the end of the animation, reset everything
-  if (aniStep == 8) {
+  if (aniStep == 3) {
     keys[keysId] = '0';
     steps[keysId] = 0;
   }
@@ -161,16 +147,16 @@ void crossIn(int aniStep, int keysId){
 }
 
 void crossOut(int aniStep, int keysId){
-  lc.setLed(0, aniStep, aniStep, true);
-  lc.setLed(0, 7-aniStep, aniStep, true);
-  lc.setLed(0, aniStep, 7-aniStep, true);
-  lc.setLed(0, 7-aniStep,7-aniStep, true);
+  lc.setLed(0, 4+aniStep, 4+aniStep, true);
+  lc.setLed(0, 3-aniStep, 3-aniStep, true);
+  lc.setLed(0, 4+aniStep, 3-aniStep, true);
+  lc.setLed(0, 3-aniStep, 4+aniStep, true);
 
     // Adds to the frame count
   steps[keysId] += 1;
 
   // If the the frame count reaches the end of the animation, reset everything
-  if (aniStep == 8) {
+  if (aniStep == 4) {
     keys[keysId] = '0';
     steps[keysId] = 0;
   }
@@ -194,11 +180,11 @@ void laserIn(int aniStep, int keysId){
   lc.setLed(0, 7-aniStep, 3, true);
   lc.setLed(0, 7-aniStep, 4, true);
 
-    // Adds to the frame count
+  // Adds to the frame count
   steps[keysId] += 1;
 
   // If the the frame count reaches the end of the animation, reset everything
-  if (aniStep == 8) {
+  if (aniStep == 3) {
     keys[keysId] = '0';
     steps[keysId] = 0;
   }
@@ -214,61 +200,46 @@ void laserOut(int aniStep, int keysId){
    */
   
   //Vertical Up
-  lc.setLed(0, 3, aniStep, true);
-  lc.setLed(0, 4, aniStep, true);
+  lc.setLed(0, 3-aniStep, 3, true);
+  lc.setLed(0, 3-aniStep, 4, true);
 
   //Vertical Down
-  lc.setLed(0, 3, 7-aniStep, true);
-  lc.setLed(0, 4, 7-aniStep, true);
+  lc.setLed(0, 4+aniStep, 3, true);
+  lc.setLed(0, 4+aniStep, 4, true);
 
   //Left in
-  lc.setLed(0, aniStep, aniStep, true);
-  lc.setLed(0, aniStep, aniStep, true);
+  lc.setLed(0, 3, 4+aniStep, true);
+  lc.setLed(0, 4, 4+aniStep, true);
 
-  //Vertical Up
-  lc.setLed(0, 7-aniStep, aniStep, true);
-  lc.setLed(0, 7-aniStep, aniStep, true);
+  //Right in
+  lc.setLed(0, 3, 3-aniStep, true);
+  lc.setLed(0, 4, 3-aniStep, true);
 
   // Adds to the frame count
   steps[keysId] += 1;
 
   // If the the frame count reaches the end of the animation, reset everything
-  if (aniStep == 8) {
+  if (aniStep == 4) { // The number here should correlate to the end of the frame loop
     keys[keysId] = '0';
     steps[keysId] = 0;
+    // Resets list
   }
   
 }
 
-void spiralIn(int aniSpeed){
-  for(int i = 0; i < 4; i++){
-    //Top left to top right
-    for(int j = 0; j < (4-i)*2-1; j++){
-      lc.clearDisplay(0);
-      lc.setLed(0, i, i+j, true);
-      delay(aniSpeed);
-    }
+void spiralIn(int aniStep, int keysId) {
+  Serial.print(spiralArray[aniStep][0]);
+  Serial.print(", ");
+  Serial.println(spiralArray[aniStep][1]);
+  lc.setLed(0, spiralArray[aniStep][0], spiralArray[aniStep][1], true);
+    
+  // Adds to the frame count
+  steps[keysId] += 1;
 
-    //Top right to bottom right
-    for(int j = 0; j < (4-i)*2-1; j++){
-      lc.clearDisplay(0);
-      lc.setLed(0, i+j, 7-i, true);
-      delay(aniSpeed);
-    }
-
-    //Bottom Right to bottom left
-    for(int j = 0; j < (4-i)*2-1; j++){
-      lc.clearDisplay(0);
-      lc.setLed(0, 7-i, 7-i-j, true);
-      delay(aniSpeed);
-    }
-
-    //Bottom left to top left
-    for(int j = 0; j < (4-i)*2-1; j++){
-      lc.clearDisplay(0);
-      lc.setLed(0, 8-1-i-j, i, true);
-      delay(aniSpeed);
-    }
+  // If the the frame count reaches the end of the animation, reset everything
+  if (aniStep == 63) { // The number here should correlate to the end of the frame loop
+    keys[keysId] = '0';
+    steps[keysId] = 0;
+    // Resets list
   }
 }
-
